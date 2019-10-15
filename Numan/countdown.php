a9093e67682aede2dd37b47d0c2297314e00bf1b
NumanQureshi <51986730+NumanQureshi@users.noreply.github.com>
2019-10-15 11:56:12 +0530

    Create countdown.php

---
+++ Numan/countdown.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  </head>
+<style>
+#timer{
+  background-color: black;
+  color: white;
+  font-size: 50px;
+  border: 5px solid red;
+  border-radius: 10px;
+}
+
+</style>
+<body>
+
+ <div class="container-fluid">
+   <div class="row">
+     <div class="col-lg-10">
+
+     </div>
+     <div class="col-lg-2">
+       <div><span id="timer" class=""></span></div>
+
+       <script>
+       document.getElementById('timer').innerHTML =
+       003 + ":" + 30;
+       startTimer();
+
+       function startTimer() {
+         var presentTime = document.getElementById('timer').innerHTML;
+         var timeArray = presentTime.split(/[:]+/);
+         var m = timeArray[0];
+         var s = checkSecond((timeArray[1] - 1));
+         if(s==59){m=m-1}
+         if(m<0){alert('timer completed');window.location.href = 'myscore.php';}
+
+
+         document.getElementById('timer').innerHTML =
+           m + ":" + s;
+         console.log(m)
+         setTimeout(startTimer, 1000);
+       }
+
+       function checkSecond(sec) {
+         if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+         if (sec < 0) {sec = "59"};
+         return sec;
+       }
+       </script>
+     </div>
+   </div>
+ </div>
+</body>
+</html>
